312ede080c42d92fdc1bd4d1dd10199ed09d7c31
Trevor_143 <trevorarapu2@gmail.com>
2023-08-31 12:11:12 -0400

    now using ejs

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 const express = require('express');
-
 const app = express();
+
+//register view engine
+app.set('view engine', 'ejs');
+
 
 //litsening
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname })
+    const blogs = [
+        { title: 'Sleeping', snippet: 'This is the best way to sleep' },
+        { title: 'Walking', snippet: 'This is the best way to walk' },
+        { title: 'Running', snippet: 'This is the best way to run' }
+    ]
+    res.render('index', {
+        title: 'Home',
+        blogs
+    });
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about', { 
+        title: 'Learn more'
+     });
 })
 
-//redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {
+        title: 'Add new Blog'
+    });
 })
 
 //404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
+    res.status(404).render('404', {
+        title: 'Error'
+    })
 })
